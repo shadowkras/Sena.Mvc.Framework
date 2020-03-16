29ee6a1730267dcf1737775926b0e3a9ea854ddb
shadowkras <33266897+shadowkras@users.noreply.github.com>
2020-03-16 13:24:29 -0400

    Add files via upload

---
+++ Vigente.Framework.Views/Attributes/GoogleReCaptchaValidationAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+
+namespace Vigente.Framework.Views.Attributes
+{
+    /// <summary>
+    /// Attributo de validação de reCAPTCHA do google.
+    /// </summary>
+    public class GoogleReCaptchaValidationAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Verifica se o reCAPTCHA é válido.
+        /// </summary>
+        /// <param name="value">Resposta do captcha.</param>
+        /// <param name="validationContext">Contexto de validação.</param>
+        /// <returns></returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Lazy<ValidationResult> errorResult = new Lazy<ValidationResult>(() => new ValidationResult("Google reCAPTCHA inválido", new String[] { validationContext.MemberName }));
+
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return errorResult.Value;
+            }
+
+            IConfiguration configuration = (IConfiguration)validationContext.GetService(typeof(IConfiguration));
+            string reCaptchResponse = value.ToString();
+            string reCaptchaSecret = configuration.GetValue<string>("GoogleReCaptcha:SecretKey");
+
+            HttpClient httpClient = new HttpClient();
+            var httpResponse = httpClient.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret={reCaptchaSecret}&response={reCaptchResponse}").Result;
+            if (httpResponse.StatusCode != HttpStatusCode.OK)
+            {
+                return errorResult.Value;
+            }
+
+            string jsonResponse = httpResponse.Content.ReadAsStringAsync().Result;
+            dynamic jsonData = JObject.Parse(jsonResponse);
+            if (jsonData.success != true.ToString().ToLower())
+            {
+                return errorResult.Value;
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Vigente.Framework.Views/Controllers/BaseApiController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Vigente.Framework.Core.Extensions;
+
+namespace Vigente.Framework.Views.Controllers
+{
+    public class BaseApiController : Controller
+    {
+        /// <summary>
+        /// Retorna o nome da controller sem o texto "Controller".
+        /// </summary>
+        protected   string ControllerName => this.GetType().Name.Replace("Controller", string.Empty);
+
+        #region Construtor
+
+        protected BaseApiController()
+        { }
+
+        #endregion
+
+        #region Retorno padrão de API
+
+        /// <summary>
+        /// Retorno em Json para métodos de API.
+        /// </summary>
+        /// <param name="sucesso">Operação executada com sucesso ou não.</param>
+        /// <param name="mensagem">Mensagem de retorno para o usuário.</param>
+        /// <param name="dados">Objeto de retorno para o usuário.</param>
+        /// <returns></returns>
+        public IActionResult RetornoApi(bool sucesso = false, string mensagem = "", object dados = null)
+        {
+            if (System.Diagnostics.Debugger.IsAttached == true)
+            {
+                return new ViewModels.RetornoApiViewModel
+                {
+                    Sucesso = sucesso,
+                    Mensagem = mensagem, //TODO pegar os erros da Model
+                    Dados = dados,
+                }.RetornoJson();
+            }
+            else
+            {
+                return new ViewModels.RetornoApiViewModel
+                {
+                    Sucesso = sucesso,
+                    Mensagem = mensagem,
+                    Dados = dados,
+                }.RetornoJson();
+            }
+        }
+
+        #endregion
+
+        #region Retorna Mensagem de Erro (API)
+
+        public IActionResult RetornaErrorApi(Exception ex, string mensagem = null)
+        {
+            if (System.Diagnostics.Debugger.IsAttached == true)
+            {
+                return new ViewModels.RetornoApiViewModel
+                {
+                    Sucesso = false,
+                    Mensagem = mensagem ?? "Ocorreu um erro.",
+                    Dados = ex.GetMessageList(),
+                }.RetornoJson();
+            }
+            else
+            {
+                return new ViewModels.RetornoApiViewModel
+                {
+                    Sucesso = false,
+                    Mensagem = mensagem ?? "Ocorreu um erro.",
+                }.RetornoJson();
+            }
+        }
+
+        #endregion
+    }
+}
