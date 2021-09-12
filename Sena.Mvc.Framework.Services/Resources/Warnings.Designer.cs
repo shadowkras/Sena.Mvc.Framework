e9b998484e4bb6a083956e8df9d06d4c4e43e1c5
Leonardo Sena <shadow_kras@hotmail.com>
2021-09-12 19:18:14 -0400

    Translated assembly references.

--- Sena.Mvc.Framework.Services/Resources/Warnings.Designer.cs
+++ Sena.Mvc.Framework.Services/Resources/Warnings.Designer.cs
@@ -39,7 +39,7 @@ namespace Sena.Mvc.Framework.Services.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sena.Mvc.Framework.Services.Resources.Criticas", typeof(Warnings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sena.Mvc.Framework.Services.Resources.Warnings", typeof(Warnings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
