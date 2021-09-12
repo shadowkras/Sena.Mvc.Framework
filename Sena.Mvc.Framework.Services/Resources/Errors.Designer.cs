d0d4d119c46e58373dc52e1221e6c059deefe651
Leonardo Sena <shadow_kras@hotmail.com>
2021-09-12 19:08:28 -0400

    Corrected an assembly reference.

--- Sena.Mvc.Framework.Services/Resources/Errors.Designer.cs
+++ Sena.Mvc.Framework.Services/Resources/Errors.Designer.cs
@@ -39,7 +39,7 @@ namespace Sena.Mvc.Framework.Services.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Vigente.Framework.Services.Resources.Erros", typeof(Errors).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sena.Mvc.Framework.Services.Resources.Erros", typeof(Errors).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
